ad6f0f4cbd60b7ab23db421e29ef593ac31283cd
Robert de Vries <r.devries@robbytu.net>
2014-11-18 13:25:01 +0100

    + Added the base Persoon class

---
+++ src/main/java/net/robbytu/hanze/kantine/Persoon.java
@@ -0,0 +1,22 @@
+package net.robbytu.hanze.kantine;/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Persoon
+ *
+ * @author R. de Vries <r.devries@robbytu.net>
+ * @version 1.0.0
+ * @date 18-11-14
+ * @copyright 2014 RobbytuProjects
+ * @license MIT License
+ */
+public class Persoon {
+
+    int BSN;
+    String voornaam;
+    String achternaam;
+    int dag;
+    int maand;
+    int jaar;
+    char geslacht;
+
+}
