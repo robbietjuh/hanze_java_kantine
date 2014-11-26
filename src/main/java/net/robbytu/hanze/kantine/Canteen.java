749284ec8e1aa5f4a293766e2a5e4188504b4a76
Robert de Vries <r.devries@robbytu.net>
2014-11-26 15:14:49 +0100

    + Documentation for the Canteen class

--- src/main/java/net/robbytu/hanze/kantine/Canteen.java
+++ src/main/java/net/robbytu/hanze/kantine/Canteen.java
@@ -14,11 +14,17 @@ public class Canteen {
     private CheckoutLine checkoutLine;
     private CashRegister cashRegister;
 
+    /**
+     * Initializes a new instance of the Canteen class
+     */
     public Canteen() {
         this.checkoutLine = new CheckoutLine();
         this.cashRegister = new CashRegister(this.checkoutLine);
     }
 
+    /**
+     * Adds a new person in line, gives them a Tray and two Articles
+     */
     public void addInLine() {
         // Create new person and assign tray
         Person personInLine = new Person();
@@ -32,6 +38,9 @@ public class Canteen {
         this.checkoutLine.addPerson(personInLine);
     }
 
+    /**
+     * Processes the check out line
+     */
     public void processCheckoutLine() {
         while(this.checkoutLine.isPersonInLine()) {
             Person personToProcess = this.checkoutLine.getFirstPersonInLine();
@@ -39,14 +48,25 @@ public class Canteen {
         }
     }
 
+    /**
+     * Returns the amount of money in the cash register
+     * @return Amount of money in the cash register
+     */
     public double getAmountOfMoneyInCashRegister() {
         return this.cashRegister.getAmountOfMoney();
     }
 
+    /**
+     * Returns the amount of Articles that have passed the cash register
+     * @return Amount of Articles that have passed the cash register
+     */
     public int getAmountOfArticles() {
         return this.cashRegister.getAmountOfArticles();
     }
 
+    /**
+     * Resets the cash register
+     */
     public void resetCashRegister() {
         this.cashRegister.resetRegister();
     }
