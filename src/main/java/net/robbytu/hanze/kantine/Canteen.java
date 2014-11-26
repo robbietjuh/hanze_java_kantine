88bcc8d8c3e200f24a9844802406744c7d5ff0d7
Robert de Vries <r.devries@robbytu.net>
2014-11-26 14:33:54 +0100

    + Added Canteen class

---
+++ src/main/java/net/robbytu/hanze/kantine/Canteen.java
@@ -0,0 +1,14 @@
+package net.robbytu.hanze.kantine;/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Canteen
+ *
+ * @author R. de Vries <r.devries@robbytu.net>
+ * @version 1.0.0
+ * @date 26-11-14
+ * @copyright 2014 RobbytuProjects
+ * @license MIT License
+ */
+public class Canteen {
+
+}
