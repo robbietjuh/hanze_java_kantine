25985e0dd75dbae12e0456f01efe9fdc18f3e892
Dylan <dylan@vossystems.nl>
2014-11-26 15:52:37 +0100

    + Changed Arraylist in Tray to a Stack and Changed ArrayList in CheckoutLine to LinkedList

--- src/main/java/net/robbytu/hanze/kantine/CheckoutLine.java
+++ src/main/java/net/robbytu/hanze/kantine/CheckoutLine.java
@@ -1,18 +1,19 @@
 package net.robbytu.hanze.kantine;
 
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Created by Dylan on 26-11-2014.
  */
 public class CheckoutLine {
 
-    private ArrayList<Person> peopleInLine;
+    private LinkedList<Person>  peopleInLine;
     /**
      * Initializes a new CheckoutLine instance
      */
     public CheckoutLine() {
-        this.peopleInLine = new ArrayList<Person>();
+        //this.peopleInLine = new ArrayList<Person>();
+        this.peopleInLine = new LinkedList<Person>();
     }
 
     /**
@@ -20,21 +21,14 @@ public class CheckoutLine {
     * @param person Person to add
     */
     public void addPerson(Person person) {
-        this.peopleInLine.add(person);
+        this.peopleInLine.addLast(person);
     }
     /**
      * Returns the first person in line. If no one is in line, returns null.
      * @return Person object or null
      */
     public Person getFirstPersonInLine() {
-        if(this.isPersonInLine()) {
-            Person firstInLine = this.peopleInLine.get(0);
-            this.peopleInLine.remove(0);
-
-            return firstInLine;
-        }
-
-        return null;
+        return this.peopleInLine.pollFirst();
     }
     /**
      * Checks wether there are people in line or not.
