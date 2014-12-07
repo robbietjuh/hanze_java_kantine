783824527e41f7b825e2140c9923f7a0b9aa0c35
Robert de Vries <r.devries@robbytu.net>
2014-12-07 19:31:25 +0100

    + Code cleanup and refactoring in CanteenSupply

--- src/main/java/net/robbytu/hanze/kantine/CanteenSupply.java
+++ src/main/java/net/robbytu/hanze/kantine/CanteenSupply.java
@@ -19,14 +19,14 @@ public class CanteenSupply {
      * have to be equal;
      */
     public CanteenSupply(String[] articlename, double[] price, int[] quantity) {
-        supply=new HashMap<String, ArrayList<Article>>();
-        for(int i=0;i<articlename.length;i++)
+        supply = new HashMap<String, ArrayList<Article>>();
+        for(int i = 0; i < articlename.length; i++)
         {
-            ArrayList<Article> articles=new ArrayList<Article>();
-            for(int j=0;j<quantity[i];j++)
-            {
+            ArrayList<Article> articles = new ArrayList<Article>();
+
+            for(int j = 0; j < quantity[i]; j++)
                 articles.add(new Article(articlename[i], price[i]));
-            }
+
             supply.put(articlename[i], articles);
         }
     }
@@ -40,32 +40,26 @@ public class CanteenSupply {
     }
 
     /**
-     * Private methode om een Artikel van de stapel artikelen af te pakken.
-     * Retourneert null als de stapel leeg is.
+     * Private method to get one Article from the stack of articles.
+     * Returns null of there are no Articles on the stack.
      */
     private Article getArticle(ArrayList<Article> stack) {
-        if (stack==null) {
-            return null;
-        }
-        if (stack.size()==0)
-        {
-            return null;
-        }
+        if(stack == null) return null;
+        if (stack.size() == 0) return null;
         else
         {
-            Article a=stack.get(0);
+            Article article = stack.get(0);
             stack.remove(0);
-            return a;
+            return article;
         }
     }
 
     /**
-     * Publieke methode om een artikel via naam van de stapel te pakken.
-     * Retouneert null als artikel niet bestaat of niet op voorraad is.
-     * @param name (van artikel)
-     * @return artikel (of null)
+     * Public method to fetch an article by name. Returns null of the article is not available
+     * @param name Name of the article
+     * @return Article The Article object or null
      */
     public Article getArticle(String name) {
-        return getArticle(getArrayList(name));
+        return this.getArticle(this.getArrayList(name));
     }
 }
