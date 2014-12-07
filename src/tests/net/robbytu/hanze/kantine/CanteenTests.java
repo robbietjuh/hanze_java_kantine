c4abab08770e6c523091b3620f5e80cc83f35c95
Robert de Vries <r.devries@robbytu.net>
2014-12-07 20:12:18 +0100

    + Totally refactored the Canteen tests to accomodate to the new way to add people to the checkout line

--- src/tests/net/robbytu/hanze/kantine/CanteenTests.java
+++ src/tests/net/robbytu/hanze/kantine/CanteenTests.java
@@ -18,28 +18,42 @@ public class CanteenTests {
 
     @Test
     public void testCanteen() {
-        // Create a new canteen, reset the cash register and check out 1 person
+        // Create a new canteen and set everything up
         Canteen canteen = new Canteen();
+        CanteenSupply supply = new CanteenSupply(new String[] { "Apple", "Banana", "Bread" },  // Article names
+                                                 new double[] { 2.50,    3.50,     3.00    },  // Article prices
+                                                 new int[]    { 5,       2,        10      }); // Article quantities
+        canteen.setCanteenSupply(supply);
 
+        // Reset the cash register and check out 1 person
         canteen.getCashRegister().resetRegister();
-        canteen.addInLine();
+        Person person = new Person();
+        person.setTray(new Tray());
+        canteen.addInLine(person, new String[]{ "Apple", "Banana" });
         canteen.processCheckoutLine();
 
         // Check that it works
-        assertThat("The amount of money in the cash register is equal to 3.50",
-                   canteen.getCashRegister().getAmountOfMoney(), equalTo(3.50));
+        assertThat("The amount of money in the cash register is equal to 6.00",
+                   canteen.getCashRegister().getAmountOfMoney(), equalTo(6.00));
 
         assertThat("The amount of articles that passed the cash register is equal to 2",
                    canteen.getCashRegister().getAmountOfArticles(), equalTo(2));
 
         // Add two more people in line and check them out
-        canteen.addInLine();
-        canteen.addInLine();
+        Person person1 = new Person();
+        Person person2 = new Person();
+
+        person1.setTray(new Tray());
+        person2.setTray(new Tray());
+
+        canteen.addInLine(person1, new String[]{ "Apple", "Banana" });
+        canteen.addInLine(person2, new String[]{ "Bread", "Apple" });
+
         canteen.processCheckoutLine();
 
         // Check that their purchases were added to the cash register
-        assertThat("The amount of money in the cash register is equal to 10.50",
-                   canteen.getCashRegister().getAmountOfMoney(), equalTo(10.50));
+        assertThat("The amount of money in the cash register is equal to 17.50",
+                   canteen.getCashRegister().getAmountOfMoney(), equalTo(17.50));
 
         assertThat("The amount of articles that passed the cash register is equal to 6",
                    canteen.getCashRegister().getAmountOfArticles(), equalTo(6));
