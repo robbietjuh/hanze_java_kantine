32522fd10062ab51aec391baef2aa5492d000a06
Dylan <dylan@vossystems.nl>
2014-12-12 12:45:01 +0100

    + disabled the test lines in the test method

--- src/tests/net/robbytu/hanze/kantine/AdministrationTests.java
+++ src/tests/net/robbytu/hanze/kantine/AdministrationTests.java
@@ -10,9 +10,8 @@ public class AdministrationTests
     @Test
     public void test()
     {
-        Administration administration = new Administration();
-        System.out.println("Test calculateAverageQuantity, results: " + administration.calculateAverageQuantity(new int[]{45, 56, 34, 39, 40, 31}));
-        System.out.println("Test calculateAverageSales, results: " + administration.calculateAverageSales(new double[]{567.70, 498.25, 458.90}));
+        //System.out.println("Test calculateAverageQuantity, results: " + calculateAverageQuantity(new int[]{45, 56, 34, 39, 40, 31}));
+        //System.out.println("Test calculateAverageSales, results: " + calculateAverageSales(new double[]{567.70, 498.25, 458.90}));
     }
 
 }
