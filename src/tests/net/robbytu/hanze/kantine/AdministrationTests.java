0dfee0159efc7ce88d9c9d3c17e2bb0cd2fcfc50
Dylan <dylan@vossystems.nl>
2014-12-12 10:58:43 +0100

    + updated method with new values for testing

--- src/tests/net/robbytu/hanze/kantine/AdministrationTests.java
+++ src/tests/net/robbytu/hanze/kantine/AdministrationTests.java
@@ -12,7 +12,7 @@ public class AdministrationTests
     {
         Administration administration = new Administration();
         System.out.println("Test calculateAverageQuantity, results: " + administration.calculateAverageQuantity(new int[]{45, 56, 34, 39, 40, 31}));
-        System.out.println("Test calculateAverageSales, results: " + administration.calculateAverageSales(new double[]{45, 56, 34, 39, 40, 31}));
+        System.out.println("Test calculateAverageSales, results: " + administration.calculateAverageSales(new double[]{567.70, 498.25, 458.90}));
     }
 
 }
