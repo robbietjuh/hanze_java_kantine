5af80f99308dc8773eb00011a321440222333650
Dylan <dylan@vossystems.nl>
2014-12-12 10:46:23 +0100

    +  created class + added methods

---
+++ src/main/java/net/robbytu/hanze/kantine/Administration.java
@@ -0,0 +1,41 @@
+package net.robbytu.hanze.kantine;
+
+/**
+ * Created by Dylan on 12-12-2014.
+ */
+public class Administration
+{
+    /**
+     * this methode calculates the average value of the int
+     * array quantity
+     * @param quantity
+     * @return the average
+     */
+    public double calculateAverageQuantity(int[] quantity)
+    {
+        double total = 0;
+        for (int value : quantity)
+        {
+               total += value;
+        }
+        return (total / quantity.length);
+    }
+
+    /**
+     * This methode calculates the average value of the double array
+     * sales
+     * @param sales
+     * @return the average
+     */
+    public double calculateAverageSales(double[] sales)
+    {
+        double total = 0;
+        for (double value : sales)
+        {
+            total += value;
+        }
+        return (total / sales.length);
+    }
+
+
+}
