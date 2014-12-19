3a41133d2ba125b8c17eec19765a3a1e5ce46083
Robert de Vries <r.devries@robbytu.net>
2014-12-19 13:03:27 +0100

    + Automatically refill the canteen supply

--- src/main/java/net/robbytu/hanze/kantine/CanteenSupply.java
+++ src/main/java/net/robbytu/hanze/kantine/CanteenSupply.java
@@ -19,16 +19,7 @@ public class CanteenSupply {
      * have to be equal;
      */
     public CanteenSupply(String[] articlename, double[] price, int[] quantity) {
-        supply = new HashMap<String, ArrayList<Article>>();
-        for(int i = 0; i < articlename.length; i++)
-        {
-            ArrayList<Article> articles = new ArrayList<Article>();
-
-            for(int j = 0; j < quantity[i]; j++)
-                articles.add(new Article(articlename[i], price[i]));
-
-            supply.put(articlename[i], articles);
-        }
+        this.addNewSupply(articlename, price, quantity);
     }
 
     /*
@@ -41,14 +32,20 @@ public class CanteenSupply {
 
     /**
      * Private method to get one Article from the stack of articles.
-     * Returns null of there are no Articles on the stack.
+     * Adds new articles to the stack when it gets out of stock
      */
     private Article getArticle(ArrayList<Article> stack) {
         if(stack == null) return null;
-        if (stack.size() == 0) return null;
         else
         {
             Article article = stack.get(0);
+
+            if(stack.size() == 1) {
+                this.addNewSupply(new String[] { article.getName() },
+                                  new double[] { article.getPrice() },
+                                  new int[] {    10 });
+            }
+
             stack.remove(0);
             return article;
         }
@@ -61,5 +58,24 @@ public class CanteenSupply {
      */
     public Article getArticle(String name) {
         return this.getArticle(this.getArrayList(name));
+    }
+
+    /**
+     * Adds new supplies
+     * @param articlename Names of the articles to add to the supply
+     * @param price Prices of the articles to add to the supply
+     * @param quantity The quantity to put in
+     */
+    public void addNewSupply(String[] articlename, double[] price, int[] quantity) {
+        supply = new HashMap<String, ArrayList<Article>>();
+        for(int i = 0; i < articlename.length; i++)
+        {
+            ArrayList<Article> articles = new ArrayList<Article>();
+
+            for(int j = 0; j < quantity[i]; j++)
+                articles.add(new Article(articlename[i], price[i]));
+
+            supply.put(articlename[i], articles);
+        }
     }
 }
