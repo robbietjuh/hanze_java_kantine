b707065977d877a256e1891a33a6fe11bc214ced
Robert de Vries <r.devries@robbytu.net>
2015-01-05 23:40:48 +0100

    + Updated CanteenSupply's javadocs

--- src/main/java/net/robbytu/hanze/kantine/CanteenSupply.java
+++ src/main/java/net/robbytu/hanze/kantine/CanteenSupply.java
@@ -23,25 +23,32 @@ public class CanteenSupply {
         this.addNewSupply(articlename, price, quantity);
     }
 
-    /*
-     * Private methode to get the list of articles based on the name of the article
+    /**
+     * Private method to get the list of articles based on the name of the article
      * returns null if the article doesn't exist.
+     * @param productname Name of the product to fetch
+     * @return An array of articles
      */
     private ArrayList<Article> getArrayList(String productname) {
         return supply.get(productname);
     }
 
+
     /**
      * Private method to get one Article from the stack of articles.
      * Adds new articles to the stack when it gets out of stock
+     * @param stack The stack of articles to pull the article from
+     * @return The requested article or null if the stack is null
      */
     private Article getArticle(ArrayList<Article> stack) {
         if(stack == null) return null;
         else
         {
+            // Fetch an article
             Article article = stack.get(0);
             stack.remove(0);
 
+            // Fill the supply with new articles when the stack is empty
             if(stack.size() == 0) {
                 this.addNewSupply(new String[] { article.getName() },
                                   new double[] { article.getPrice() },
@@ -55,7 +62,7 @@ public class CanteenSupply {
     /**
      * Public method to fetch an article by name. Returns null of the article is not available
      * @param name Name of the article
-     * @return Article The Article object or null
+     * @return The Article object or null
      */
     public Article getArticle(String name) {
         return this.getArticle(this.getArrayList(name));
