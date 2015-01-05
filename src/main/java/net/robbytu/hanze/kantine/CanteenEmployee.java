f8fba9d0218010a461416e4434074f4373f1b27d
Robert de Vries <r.devries@robbytu.net>
2015-01-05 23:34:20 +0100

    + Refactored the CanteenEmployee constructors

--- src/main/java/net/robbytu/hanze/kantine/CanteenEmployee.java
+++ src/main/java/net/robbytu/hanze/kantine/CanteenEmployee.java
@@ -16,43 +16,47 @@ public class CanteenEmployee extends Person
      */
     public CanteenEmployee()
     {
+        super();
+
         Random random = new Random();
 
         this.employeeNumber = random.nextInt(9999 - 1000) + 1000;
         this.canUseCashRegister = (random.nextInt(2) == 0);
     }
 
-    public CanteenEmployee(int employeeNumber, boolean canUseCashRegister,  int bsn, String firstname, String lastname, int day, int month, int year, char gender)
-    {
-        super();
-
-        this.setBsn(bsn);
-        this.setFirstname(firstname);
-        this.setLastname(lastname);
-        this.setBirthdate(day, month, year);
-        this.setGender(gender);
+    /**
+     * Initializes a new instance of the CanteenEmployee class with specified properties
+     * @param employeeNumber Unique ID for this employee
+     * @param canUseCashRegister Wether or not this employee is allowed to use the cash register
+     * @param bsn National security number
+     * @param firstname First name
+     * @param lastname Last name
+     * @param day Birthdate -- day
+     * @param month Birthdate -- month
+     * @param year Birthdate -- year
+     * @param gender Gender
+     */
+    public CanteenEmployee(int employeeNumber, boolean canUseCashRegister,  int bsn, String firstname,
+                           String lastname, int day, int month, int year, char gender) {
+        super(bsn, firstname, lastname, day, month, year, gender);
 
         this.employeeNumber = employeeNumber;
         this.canUseCashRegister = canUseCashRegister;
     }
 
-    public int getEmployeeNumber()
-    {
+    public int getEmployeeNumber() {
         return this.employeeNumber;
     }
 
-    public void setEmployeeNumber(int employeeNumber)
-    {
+    public void setEmployeeNumber(int employeeNumber) {
         this.employeeNumber = employeeNumber;
     }
 
-    public boolean getCanUseCashRegister()
-    {
+    public boolean getCanUseCashRegister() {
         return this.canUseCashRegister;
     }
 
-    public void setCanUseCashRegister(boolean canUseCashRegister)
-    {
+    public void setCanUseCashRegister(boolean canUseCashRegister) {
         this.canUseCashRegister = canUseCashRegister;
     }
 
@@ -61,8 +65,7 @@ public class CanteenEmployee extends Person
      * and prints employeeNumber and canUseCashRegister
      */
     @Override
-    public void printDetails()
-    {
+    public void printDetails() {
         System.out.println("Details for the canteen employee with employeenumber: " + this.employeeNumber);
         System.out.println("This employee " + (canUseCashRegister ? "is" : "is not") + " allowed to operate the cash register.");
     }
