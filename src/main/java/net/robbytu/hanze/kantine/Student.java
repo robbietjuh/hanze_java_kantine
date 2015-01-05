06b30f860707aa66cd500eb373d6303c5ce43dd3
Robert de Vries <r.devries@robbytu.net>
2015-01-05 23:55:26 +0100

    + Refactored and cleaned the Student class

--- src/main/java/net/robbytu/hanze/kantine/Student.java
+++ src/main/java/net/robbytu/hanze/kantine/Student.java
@@ -7,55 +7,75 @@ import java.util.Random;
  */
 public class Student extends Person
 {
+    private static final String[] STUDIES = {"Software Engineering", "Game Development",
+                                             "Teacher Study","Economics", "Actor"};
+
     private int studentNumber;
     private String study;
-
 
     /**
      * Initializes a new instance of the Student class
      * without any parameters
      */
-    public Student()
-    {
-        Random random = new Random();
-        this.studentNumber = random.nextInt(9999 - 1000) + 1000;
-        int tempNumber = random.nextInt(5);
-        String[] studies = {"Software Engineering", "Game Development", "Teacher Study", "Economics", "Actor"};
-        this.study = studies[tempNumber];
-    }
-
-    public Student(int studentNumber, String study, int bsn, String firstname, String lastname, int day, int month, int year, char gender)
-    {
+    public Student() {
         super();
 
-        setBsn(bsn);
-        setFirstname(firstname);
-        setLastname(lastname);
-        setBirthdate(day, month, year);
-        setGender(gender);
+        Random random = new Random();
+
+        this.studentNumber = random.nextInt(9999 - 1000) + 1000;
+        this.study = STUDIES[random.nextInt(5)];
+    }
+
+    /**
+     * Initializes a new instance of the Student class with the specified properties
+     * @param studentNumber Unique Student ID
+     * @param study The name of the study this person is subscribed to
+     * @param bsn National security number
+     * @param firstname First name
+     * @param lastname Last name
+     * @param day Birthdate -- day
+     * @param month Birthdate -- month
+     * @param year Birthdate -- year
+     * @param gender Gender
+     */
+    public Student(int studentNumber, String study, int bsn, String firstname,
+                   String lastname, int day, int month, int year, char gender) {
+        super(bsn, firstname, lastname, day, month, year, gender);
 
         this.studentNumber = studentNumber;
         this.study = study;
     }
 
-    public String getStudy()
-    {
-        return study;
+    /**
+     * Returns this student's unique ID
+     * @return The student's unique ID
+     */
+    public int getStudentNumber() {
+        return this.studentNumber;
     }
 
-    public void setStudy(String study)
-    {
-        this.study = study;
-    }
-
-    public int getStudentNumber()
-    {
-        return studentNumber;
-    }
-
-    public void setStudentNumber(int studentNumber)
-    {
+    /**
+     * Sets this student's unique ID
+     * @param studentNumber This student's unique ID
+     */
+    public void setStudentNumber(int studentNumber) {
         this.studentNumber = studentNumber;
+    }
+
+    /**
+     * Returns the name of the study this person is subscribed to
+     * @return The name of the study this person is subscribed to
+     */
+    public String getStudy() {
+        return this.study;
+    }
+
+    /**
+     * Sets the name of the study this person is subscribed to
+     * @param study The name of the study this person is subscribed to
+     */
+    public void setStudy(String study) {
+        this.study = study;
     }
 
     /**
@@ -63,9 +83,8 @@ public class Student extends Person
      * and prints studentNumber and study
      */
     @Override
-    public void printDetails()
-    {
+    public void printDetails() {
         System.out.println("Details for the student with studentnumber: " + this.studentNumber);
-        System.out.println("Study: " + this.study);
+        System.out.println("This person is studying " + this.study);
     }
 }
