6dc8b44f3ae78880614aaf7fcc9b7ee0822e7888
Robert de Vries <r.devries@robbytu.net>
2015-01-05 23:08:39 +0100

    + Implemented unit tests for the Administration class

--- src/tests/net/robbytu/hanze/kantine/AdministrationTests.java
+++ src/tests/net/robbytu/hanze/kantine/AdministrationTests.java
@@ -2,16 +2,30 @@ package net.robbytu.hanze.kantine;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
 /**
  * Created by Dylan on 12-12-2014.
  */
 public class AdministrationTests
 {
     @Test
-    public void test()
+    public void testAverageQuantity()
     {
-        //System.out.println("Test calculateAverageQuantity, results: " + calculateAverageQuantity(new int[]{45, 56, 34, 39, 40, 31}));
-        //System.out.println("Test calculateAverageSales, results: " + calculateAverageSales(new double[]{567.70, 498.25, 458.90}));
+        double averageQuantity = Administration.calculateAverageQuantity(new int[]{45, 56, 34, 39, 40, 31});
+
+        assertThat("The average quantity matches",
+                   averageQuantity, equalTo(40.833333333333336));
+    }
+
+    @Test
+    public void testAverageSales()
+    {
+        double averageQuantity = Administration.calculateAverageSales(new double[]{567.70, 498.25, 458.90});
+
+        assertThat("The average sales matches",
+                averageQuantity, equalTo(508.2833333333333));
     }
 
 }
