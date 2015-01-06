bbd93beac6d4618517103b2c4884e5923e5d3e03
Robert de Vries <r.devries@robbytu.net>
2015-01-06 03:09:09 +0100

    + Implemented new unit tests for the cash register

--- src/tests/net/robbytu/hanze/kantine/CashRegisterTests.java
+++ src/tests/net/robbytu/hanze/kantine/CashRegisterTests.java
@@ -58,6 +58,11 @@ public class CashRegisterTests {
         Person person = new Person(1234, "Robert", "de Vries", 11, 10, 1996, 'M');
         person.setTray(new Tray());
 
+        // Assign a wallet with 20 euros in it
+        PaymentMethod wallet = new CashPaymentMethod();
+        wallet.setBalance(20.0);
+        person.setPaymentMethod(wallet);
+
         // Now assign a product that's way too expensive
         tray = person.getTray();
         tray.addArticle(new Article("Capybara", 85.00)); // Check out a capybara at http://kwieb.com !
@@ -70,5 +75,65 @@ public class CashRegisterTests {
 
         assertThat("There are 0 euros in the cash register",
                    cashregister.getAmountOfMoney(), equalTo(0.0));
+    }
+
+    @Test
+    public void testDiscountWithLimitCheckout() {
+        // Set up initial environment
+        CashRegister cashregister = new CashRegister();
+        cashregister.resetRegister();
+        Tray tray;
+
+        // Create a person and give it a tray -- in this case a teacher as we know they get discounts with limits
+        Teacher person = new Teacher();
+        person.setTray(new Tray());
+
+        // Assign a wallet with 100 euros in it
+        PaymentMethod wallet = new CashPaymentMethod();
+        wallet.setBalance(100.0);
+        person.setPaymentMethod(wallet);
+
+        // Now assign a product
+        tray = person.getTray();
+        tray.addArticle(new Article("Banana", 100.00));
+
+        // Checkout
+        cashregister.checkout(person);
+
+        assertThat("1 article passed the cash register",
+                cashregister.getAmountOfArticles(), equalTo(1));
+
+        assertThat("There are 99 euros in the cash register because of the discount limit of 1 euro",
+                cashregister.getAmountOfMoney(), equalTo(99.0));
+    }
+
+    @Test
+    public void testDiscountWithoutLimitCheckout() {
+        // Set up initial environment
+        CashRegister cashregister = new CashRegister();
+        cashregister.resetRegister();
+        Tray tray;
+
+        // Create a person and give it a tray -- in this case a teacher as we know they get discounts without limits!
+        CanteenEmployee person = new CanteenEmployee();
+        person.setTray(new Tray());
+
+        // Assign a wallet with 100 euros in it
+        PaymentMethod wallet = new CashPaymentMethod();
+        wallet.setBalance(100.0);
+        person.setPaymentMethod(wallet);
+
+        // Now assign a product
+        tray = person.getTray();
+        tray.addArticle(new Article("Banana", 100.00));
+
+        // Checkout
+        cashregister.checkout(person);
+
+        assertThat("1 article passed the cash register",
+                cashregister.getAmountOfArticles(), equalTo(1));
+
+        assertThat("There are 65 euros in the cash register because of the discount of 35% percent without limits",
+                cashregister.getAmountOfMoney(), equalTo(65.0));
     }
 }
