66c1199e68120a657e99046eaccfbc9a2d1098f6
Robert de Vries <r.devries@robbytu.net>
2015-01-07 12:35:29 +0100

    + Updated javadocs

--- src/main/java/net/robbytu/hanze/kantine/TooLittleMoneyException.java
+++ src/main/java/net/robbytu/hanze/kantine/TooLittleMoneyException.java
@@ -5,16 +5,27 @@ package net.robbytu.hanze.kantine;
  */
 public class TooLittleMoneyException extends Exception
 {
+    /**
+     * Too low a balance
+     */
     public TooLittleMoneyException()
     {
         super();
     }
 
+    /**
+     * Too low a balance
+     * @param e Exception
+     */
     public TooLittleMoneyException(Exception e)
     {
         super(e);
     }
 
+    /**
+     * Too low a balance
+     * @param Message Message to throw
+     */
     public TooLittleMoneyException(String Message)
     {
         super(Message);
